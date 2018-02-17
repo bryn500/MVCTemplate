3fc79b2c0770c14c9c07880f8cf0e9c91a804af6
Bryn Thomas <bryn500@outlook.com>
2018-02-17 15:11:53 +0000

    Updates
    Removed Application Insights
    Refined HTTP headers

--- Global.asax.cs
+++ Global.asax.cs
@@ -14,11 +14,37 @@ namespace $safeprojectname$
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-            //// Used to help clear output cache
-            //HttpRuntime.Cache.Insert("Pages", DateTime.Now);
+            // Used to help clear output cache
+            HttpRuntime.Cache.Insert("Pages", DateTime.Now);
 
-            //// Remove X-AspNetMvc-Version HTTP header 
-            //MvcHandler.DisableMvcResponseHeader = true;
+            // Remove X-AspNetMvc-Version HTTP header 
+            MvcHandler.DisableMvcResponseHeader = true;
+        }
+
+        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            // Remove Server HTTP Header
+            HttpContext.Current.Response.Headers.Remove("Server");
+        }
+
+        public override string GetVaryByCustomString(HttpContext context, string custom)
+        {
+            if (String.IsNullOrEmpty(custom))
+                return base.GetVaryByCustomString(context, custom);
+
+            // do not output cache: return Guid.NewGuid().ToString();
+
+            // vary cache based on custom requirements
+            List<string> retVal = new List<string>();
+            List<string> customParts = custom.Split(new char[] { ',', ';' }).ToList();
+
+            foreach (var customPart in customParts)
+            {
+                // if we are meant to vary by part
+                // add vary string to retval
+            }
+
+            return string.Join(";", retVal);
         }
     }
 }
